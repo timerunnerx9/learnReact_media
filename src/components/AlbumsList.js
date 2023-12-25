a69004addafcc6f894916bce2e93fb5858d6258c
dave cen <davecen9@gmail.com>
2023-12-25 08:26:36 -0500

    save

--- src/components/AlbumsList.js
+++ src/components/AlbumsList.js
@@ -1,8 +1,34 @@
 import { useFetchAlbumsQuery } from "../store";
+import Skeleton from "./Skeleton";
+import ExpandablePanel from "./ExpandablePanel";
+import Button from "./Button";
 
 function AlbumsList({ user }) {
-  const {data,error, isLoading} =  useFetchAlbumsQuery();
-  return <div>Albums for {user.name}</div>;
+  const { data, error, isLoading } = useFetchAlbumsQuery(user);
+
+  let content;
+
+  if (isLoading) {
+    content = <Skeleton time={3} />;
+  } else if (error) {
+    content = <div>Error Loading Albums</div>;
+  } else {
+    content = data.map((album) => {
+      const header = <div>{album.title}</div>;
+      return (
+        <ExpandablePanel key={album.id} header={header}>
+          List of photos in the album.
+        </ExpandablePanel>
+      );
+    });
+  }
+
+  return (
+    <div>
+      <div>Albums for {user.name}</div>
+      <div>{content}</div>
+    </div>
+  );
 }
 
 export default AlbumsList;
--- src/store/index.js
+++ src/store/index.js
@@ -1,22 +1,21 @@
-import { configureStore } from '@reduxjs/toolkit';
-import { usersReducer } from './slices/usersSlice';
-import { setupListeners } from '@reduxjs/toolkit/query';
-import { albumsApi } from './apis/albumsApi';
+import { configureStore } from "@reduxjs/toolkit";
+import { usersReducer } from "./slices/usersSlice";
+import { setupListeners } from "@reduxjs/toolkit/query";
+import { albumsApi } from "./apis/albumsApi";
 
 export const store = configureStore({
   reducer: {
     users: usersReducer,
-    [albumsApi.reducerPath]: albumsApi.reducer
+    [albumsApi.reducerPath]: albumsApi.reducer,
   },
-  middleware: (getDefaultMiddleware)=>{
-    return getDefaultMiddleware()
-    .concat(albumsApi.middleware);
-  }
+  middleware: (getDefaultMiddleware) => {
+    return getDefaultMiddleware().concat(albumsApi.middleware);
+  },
 });
 
 setupListeners(store.dispatch);
 
-export * from './thunks/fetchUsers';
-export * from './thunks/addUser';
-export * from './thunks/removeUser';
-export {useFetchAlbumsQuery} from '/apis/albumsApi';
+export * from "./thunks/fetchUsers";
+export * from "./thunks/addUser";
+export * from "./thunks/removeUser";
+export { useFetchAlbumsQuery } from "./apis/albumsApi";
